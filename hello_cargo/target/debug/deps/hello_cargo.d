9300addd15601bf9d8ffda0c960b710aff9a08f3
H4KB <akabaharuka@gmail.com>
2021-10-10 14:54:16 +0900

    User cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\tenni\Github\Trainig_rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\tenni\Github\Trainig_rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
